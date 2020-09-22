87ec757e304ee3c03c6ccc1119d4d8a34e18f34f
Andreas Krämer <kraemer.research@gmail.com>
2020-09-22 11:00:15 +0200

    test system object

--- openmmsystems/tests/samples/test_ala2.py
+++ openmmsystems/tests/samples/test_ala2.py
@@ -2,9 +2,11 @@
 from openmmsystems.samples import Ala2Implicit300
 
 
-def test_dataset(tmpdir):
+def test_ala2_300(tmpdir):
     dataset = Ala2Implicit300(root=tmpdir, download=True, read=True)
     assert dataset.coordinates.shape == (len(dataset), 22, 3)
     assert dataset.forces.shape == (len(dataset), 22, 3)
     assert dataset.energies.shape == (len(dataset), )
+    print(dataset.system)
+
 
