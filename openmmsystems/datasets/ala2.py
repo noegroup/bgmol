455ae1525034ba5b00ddee27a2f12b968027b75e
Andreas Krämer <kraemer.research@gmail.com>
2020-09-29 13:47:24 +0200

    added ala2 1000K

--- openmmsystems/datasets/ala2.py
+++ openmmsystems/datasets/ala2.py
@@ -3,7 +3,7 @@ from openmmsystems.datasets.base import DataSet
 from openmmsystems.api import system_by_name
 from openmmsystems.tpl.hdf5 import HDF5TrajectoryFile, load_hdf5
 
-__all__ = ["Ala2Implicit300"]
+__all__ = ["Ala2Implicit300", "Ala2Implicit1000"]
 
 
 class Ala2Implicit300(DataSet):
@@ -27,6 +27,37 @@ class Ala2Implicit300(DataSet):
     @property
     def trajectory_file(self):
         return os.path.join(self.root, "Ala2Implicit300/traj0.h5")
+
+    def read(self, n_frames=None, stride=None, atom_indices=None):
+        self.trajectory = load_hdf5(self.trajectory_file)
+        f = HDF5TrajectoryFile(self.trajectory_file)
+        frames = f.read(n_frames=n_frames, stride=stride, atom_indices=atom_indices)
+        self._energies = frames.potentialEnergy
+        self._forces = frames.forces
+        f.close()
+
+
+class Ala2Implicit1000(DataSet):
+    """AlanineDipeptideImplicit at 300 K.
+    1 ms Langevin dynamics with 1/ps friction coefficient and 2fs time step,
+    output spaced in 10 ps intervals.
+    """
+    url = "ftp://ftp.mi.fu-berlin.de/pub/cmb-data/openmmsystems/Ala2Implicit1000.tgz"
+    md5 = "9a90965254dac7440976d0d62687caed"
+    num_frames = 100000
+    size = 51071715 # in bytes
+    selection = "all"
+    openmm_version = "7.4.1"
+    date = "2020/09/29"
+
+    def __init__(self, root=os.getcwd(), download: bool = False, read: bool = False):
+        super(Ala2Implicit1000, self).__init__(root=root, download=download, read=read)
+        self._system = system_by_name("AlanineDipeptideImplicit")
+        self._temperature = 1000
+
+    @property
+    def trajectory_file(self):
+        return os.path.join(self.root, "Ala2Implicit1000/traj0.h5")
 
     def read(self, n_frames=None, stride=None, atom_indices=None):
         self.trajectory = load_hdf5(self.trajectory_file)
