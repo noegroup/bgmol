85a300aa91871a5ecc209356d958ce9afdae0022
Andreas Krämer <kraemer.research@gmail.com>
2021-01-15 14:10:52 +0100

    bugfix in new datasets

--- openmmsystems/datasets/__init__.py
+++ openmmsystems/datasets/__init__.py
@@ -1,3 +1,4 @@
 
 from .base import *
 from .ala2 import *
+from .minipeptides import *
--- openmmsystems/datasets/minipeptides.py
+++ openmmsystems/datasets/minipeptides.py
@@ -8,41 +8,7 @@ from ..systems.minipeptides import MiniPeptide
 from ..tpl.hdf5 import load_hdf5, HDF5TrajectoryFile
 
 
-class Ala2Implicit1000(DataSet):
-    """AlanineDipeptideImplicit at 300 K.
-    1 ms Langevin dynamics with 1/ps friction coefficient and 2fs time step,
-    output spaced in 10 ps intervals.
-    """
-    url = "ftp://ftp.mi.fu-berlin.de/pub/cmb-data/openmmsystems/Ala2Implicit1000.tgz"
-    md5 = "9a90965254dac7440976d0d62687caed"
-    num_frames = 100000
-    size = 51071715 # in bytes
-    selection = "all"
-    openmm_version = "7.4.1"
-    date = "2020/09/29"
-
-    def __init__(self, root=os.getcwd(), download: bool = False, read: bool = False):
-        super(Ala2Implicit1000, self).__init__(root=root, download=download, read=read)
-        self._system = MiniPeptide(
-            aminoacids="A",
-            solvated=True,
-            forcefield=["amoeba2013.xml"],
-            nonbonded_cutoff=1.2*unit.nanometer,
-            switch_distance=1.0*unit.nanometer
-        )
-        self._temperature = 300.
-
-    @property
-    def trajectory_file(self):
-        return os.path.join(self.root, "Ala2Implicit1000/traj0.h5")
-
-    def read(self, n_frames=None, stride=None, atom_indices=None):
-        self.trajectory = load_hdf5(self.trajectory_file)
-        f = HDF5TrajectoryFile(self.trajectory_file)
-        frames = f.read(n_frames=n_frames, stride=stride, atom_indices=atom_indices)
-        self._energies = frames.potentialEnergy
-        self._forces = frames.forces
-        f.close()
+__all__ = ["ASolvatedAmoeba", "ASolvatedAmber14"]
 
 
 class ASolvatedAmoeba(DataSet):
