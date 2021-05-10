9d4ea4ffdd07ddbdf529973fb02adbe961efbd51
Andreas Krämer <30830507+Olllom@users.noreply.github.com>
2021-05-10 10:16:58 +0200

    Add ala2 implicit unconstrained (#6)
    * chignolin dataset
    
    * chignolin dataset; one temperature missing
    
    * fix cgn dataset; logical dir structure on ftp
    
    * add cgn tica
    
    * fix cgn topology
    
    * remove README sections
    
    * add AImplicitUnconstrained data

--- bgmol/datasets/minipeptides.py
+++ bgmol/datasets/minipeptides.py
@@ -11,7 +11,7 @@ from ..systems.minipeptides import MiniPeptide
 from ..tpl.hdf5 import load_hdf5, HDF5TrajectoryFile
 
 
-__all__ = ["ASolvatedAmoeba", "ASolvatedAmber14", "ASolvatedAmber99"]
+__all__ = ["ASolvatedAmoeba", "ASolvatedAmber14", "ASolvatedAmber99", "AImplicitUnconstrained"]
 
 
 class ASolvatedAmoeba(DataSet):
@@ -131,6 +131,53 @@ class ASolvatedAmber99(DataSet):
     def integrator(self):
         integrator = LangevinIntegrator(300 * unit.kelvin, 1 / unit.picosecond, 0.004 * unit.picoseconds)
         integrator.setConstraintTolerance(1e-5)
+        return integrator
+
+    @property
+    def platform(self):
+        platform = Platform.getPlatformByName("CUDA")
+        platform.setPropertyDefaultValue("Precision", "mixed")
+        return platform
+
+
+class AImplicitUnconstrained(DataSet):
+    """Capped alanine in implicit solvent without bond constraints.
+    1 microsecond samples spaced in 1 ps intervals.
+    The dataset contains positions, forces, and energies.
+    """
+    url = "ftp://ftp.mi.fu-berlin.de/pub/cmb-data/bgmol/datasets/minipeptides/AImplicitUnconstrained.tgz"
+    md5 = "f18b9a9c06f3590f1632ca99161c6553"
+    num_frames = 1000000
+    size = 461080  # in bytes
+    selection = "all"
+    openmm_version = "7.5.0"
+    date = "2021/05/10"
+    author = "Andreas Krämer"
+
+    def __init__(self, root=os.getcwd(), download: bool = False, read: bool = False):
+        super().__init__(root=root, download=download, read=read)
+        self._system = MiniPeptide(
+            "A",
+            solvated=False,
+            constraints=None,
+        )
+        self._temperature = 300.
+
+    @property
+    def trajectory_file(self):
+        return os.path.join(self.root, "AImplicitUnconstrained/traj0.h5")
+
+    def read(self, n_frames=None, stride=None, atom_indices=None):
+        self.trajectory = load_hdf5(self.trajectory_file)
+        f = HDF5TrajectoryFile(self.trajectory_file)
+        frames = f.read(n_frames=n_frames, stride=stride, atom_indices=atom_indices)
+        self._energies = frames.potentialEnergy
+        self._forces = frames.forces
+        f.close()
+
+    @property
+    def integrator(self):
+        integrator = LangevinIntegrator(self.temperature * unit.kelvin, 1.0 / unit.picosecond, 1.0 * unit.femtosecond)
         return integrator
 
     @property
