a1a17e89e083ce9372d80dd924ab208ad421ddc7
Andreas Krämer <kraemer.research@gmail.com>
2021-11-18 18:35:28 +0100

    arrestin system

--- bgmol/systems/arrestin.py
+++ bgmol/systems/arrestin.py
@@ -1,58 +1,44 @@
 import os
 import tempfile
-import numpy as np
-from simtk.openmm import app
-import mdtraj as md
-from ..systems.base import OpenMMToolsTestSystem, OpenMMSystem
+from ..systems.base import OpenMMSystem
 from torchvision.datasets.utils import download_url
-import pickle5 as pickle
+
+from ..util.importing import import_openmm
+mm, unit, app = import_openmm()
 
 
-__all__ = ["ArrestinActive", "ArrestinInactive"]
+__all__ = ["ArrestinActive"]
 
 
 class ArrestinActive(OpenMMSystem):
     """
     """
+    url = "http://ftp.mi.fu-berlin.de/pub/cmb-data/bgmol/systems/arrestin/"
+
     def __init__(self,  root=tempfile.gettempdir(), download=True):
         super(ArrestinActive, self).__init__()
 
-        filename = "openmm-arrestin/arr2-active_start.pdb"
+        if download:
+            filename = self._download(root)
+        assert os.path.isfile(filename)
 
         pdb = app.PDBFile(filename)
-        ff = app.ForceField("amber99sbildn.xml", "amber96_obc.xml")
+        forcefield = app.ForceField("amber99sbildn.xml", "amber96_obc.xml")
 
-        name = 'openmm-arrestin/arr2-active_system'
-        with open(name + '.pkl', 'rb') as f:
-            system = pickle.load(f)
-            self._system = system
+        self._system = forcefield.createSystem(
+            pdb.topology,
+            nonbondedMethod=app.PME,
+            nonbondedCutoff=1 * unit.nanometer,
+            constraints=app.HBonds
+        )
 
-        self._positions = pdb.getPositions(asNumpy=True)
+        self._positions = pdb.getPositions(asNumpy=True).value_in_unit(unit.nanometer)
         self._topology = pdb.getTopology()
-        self.z_matrix = None
-        self.rigid_block = None
 
-
-class ArrestinInactive(OpenMMSystem):
-    """
-    """
-    def __init__(self,  root=tempfile.gettempdir(), download=True):
-        super(ArrestinInactive, self).__init__()
-
-        filename = "openmm-arrestin/arr2-inactive_system"
-        full_filename = os.path.join(root, filename)
-
-        pdb = app.PDBFile(filename)
-        ff = app.ForceField("amber99sbildn.xml", "amber96_obc.xml")
-
-        name = 'arrestin_system_inactive'#file doesn't exist yet
-        with open(name + '.pkl', 'rb') as f:
-            system = pickle.load(f)
-            self._system = system
-        
-        self._positions = pdb.getPositions(asNumpy=True)
-        self._topology = pdb.getTopology()
-        self.z_matrix = None
-        self.rigid_block = None
-
+    def _download(self, root):
+        md5 = "c428fe87c0e888bc19e444eef05a9b3d"
+        filename = "arr2-active_start.pdb"
+        download_url(self.url+filename, root, filename, md5)
+        local_file = os.path.join(root, filename)
+        return local_file
 
